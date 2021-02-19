06910fc5823e14a20c834dbe62b91666ad864fcd
Kaleb Day <k.day@uqconnect.edu.au>
2021-02-19 22:17:46 +1000

    add group to db

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Drinks from "./components/Drinks";
 import JoinForm from "./components/JoinForm";
-import { useState, useEffect } from "react";
 
 function App() {
   const [drinks, setDrinks] = useState([]);
+  const [groups, setGroups] = useState([]);
   const [inGroup, setInGroup] = useState(false);
 
   // Get drinks from server on load
@@ -13,13 +14,26 @@ function App() {
       const drinksFromServer = await fetchDrinks();
       setDrinks(drinksFromServer);
     };
-
     getDrinks();
+
+    const getGroups = async () => {
+      const groupsFromServer = await fetchGroups();
+      setGroups(groupsFromServer);
+    };
+    getGroups();
   }, []);
 
   // Fetch Drinks
   const fetchDrinks = async () => {
     const res = await fetch("http://localhost:5000/drinks");
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Fetch Groups
+  const fetchGroups = async () => {
+    const res = await fetch("http://localhost:5000/groups");
     const data = await res.json();
 
     return data;
@@ -35,12 +49,44 @@ function App() {
 
   const createGroup = () => {};
 
-  const joinGroup = () => {};
+  // Add Group to db
+  const addGroup = (groupName) => {
+    const groupNames = [];
+    groups.forEach(({ id, name }) => groupNames.push(name));
+
+    // Data validation
+    if (groupNames.includes(groupName)) {
+      alert("Group name already exists");
+      return;
+    }
+
+    console.log(groupName);
+
+    const group = { name: groupName };
+
+    fetch("http://localhost:5000/groups", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(group),
+    })
+      .then((response) => response.json())
+      .then((data) => setGroups([...groups, data]))
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+
+    console.log(groups);
+  };
 
   return (
     <div>
       <Header />
-      <JoinForm />
+      <JoinForm onAdd={addGroup} />
+      {groups.map((group) => (
+        <p key={group.id}>{group.name}</p>
+      ))}
     </div>
     // <div className="App">
     //   <Header />
--- src/components/JoinForm.js
+++ src/components/JoinForm.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
 
-const JoinForm = () => {
+const JoinForm = ({ onAdd }) => {
   const [groupName, setGroupName] = useState("");
-  const [name, setName] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!groupName) {
+      alert("Please enter a valid group name");
+      return;
+    }
+
+    onAdd(groupName);
+
+    setGroupName([""]);
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2>Join Group</h2>
       <label htmlFor="group-name">Group Name: </label>
       <br />
@@ -16,16 +28,7 @@ const JoinForm = () => {
         onChange={(e) => setGroupName(e.target.value)}
       />
       <br />
-      <label htmlFor="name">Name: </label>
-      <br />
-      <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
 
-      <br />
       <input type="submit" value="Join" />
     </form>
   );
